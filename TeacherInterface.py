c640fe22e2ed05b934e14e273271afd2152ba564
imchetanvij <41373648+imchetanvij@users.noreply.github.com>
2025-05-21 11:03:38 +0530

    Update TeacherInterface.py

--- TeacherInterface.py
+++ TeacherInterface.py
@@ -266,7 +266,7 @@ def main():
         work_done = st.selectbox(
             "WORK DONE IN THE CLASS",
             options=options,
-            value=str(row.get('WORK DONE IN THE CLASS','').strip(),
+            value=row.get('WORK DONE IN THE CLASS'),
             index=index,
             format_func=lambda x: picture_options.get(x, x),
             key=f"workdone_{idx}"
